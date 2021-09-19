ab2ef01efc1befd25a1dcf80fb0b4dbf2955a495
Екатерина <katerinashatunova@gmail.com>
2021-09-19 22:46:32 +0500

    base functional: interpolation, data, methods, v-on

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>Root component start!</div>
+  <button @click="addLike">Like</button>
+  <button @click="addDislike">Dislike</button>
+  <div>Count likes: <strong>{{ likes }}</strong></div>
+  <div>Count dislikes: <strong>{{ dislikes }}</strong></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
