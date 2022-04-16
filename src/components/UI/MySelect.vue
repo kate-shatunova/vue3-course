fd7418141dc614e124290180173b341e0a67841e
Екатерина <katerinashatunova@gmail.com>
2022-04-17 00:37:17 +0500

    create UI component select

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,15 @@
+<template>
+  <select>
+    <option disabled value="">Выберите из списка</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "my-select"
+}
+</script>
+
+<style scoped>
+
+</style>
