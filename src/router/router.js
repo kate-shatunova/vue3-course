b5fc0497502a4a7e9dfa0ceeb475e0cc93878e0c
Екатерина <katerinashatunova@gmail.com>
2022-04-24 23:09:32 +0500

    feat: create dinamyc navigation by ID

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,8 @@
 import Main from "../pages/Main";
 import {createRouter, createWebHistory} from "vue-router";
-import PostPage from "../pages/PostPage";
+import PostsPage from "../pages/PostsPage";
 import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
 
 const routes = [
     {
@@ -10,12 +11,16 @@ const routes = [
     },
     {
         path: "/posts",
-        component: PostPage
+        component: PostsPage
     },
     {
         path: "/about",
         component: About
     },
+    {
+        path: '/posts/:id',
+        component: PostIdPage
+    }
 ]
 
 const router = createRouter({
