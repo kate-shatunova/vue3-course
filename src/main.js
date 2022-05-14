2d5b88813aa7ebfcdbf416d9c2eeb28a9ba2bd54
Екатерина <katerinashatunova@gmail.com>
2022-05-14 20:59:24 +0500

    feat: create and register gloabl store VUEX

--- src/main.js
+++ src/main.js
@@ -2,20 +2,22 @@ import {createApp} from 'vue'
 import App from './App'
 import components from '@/components/UI'
 import router from '@/router/router';
+import store from "./store";
 import directives from '@/directives/';
 
 const app = createApp(App)
 
 // глобальная регистрация компонент
 components.forEach(component => {
-    app.component(component.name, component)
+  app.component(component.name, component)
 })
 
 // глобальная регистрация директив
 directives.forEach(directive => {
-    app.directive(directive.name, directive);
+  app.directive(directive.name, directive);
 })
 
 app
-    .use(router)
-    .mount('#app')
+  .use(router)
+  .use(store)
+  .mount('#app')
